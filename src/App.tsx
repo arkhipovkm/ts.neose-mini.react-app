e29bd38027dc1d796452bafa323298c8f640854c
Kirill Arkhipov <arkhipov.km@gmail.com>
2023-03-09 17:53:33 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import type { FC } from 'react';
+import { Col, message, Row, Switch, Typography } from 'antd';
+import 'antd/dist/reset.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sendRequest = async (path: string) => {
+  return fetch(`http://localhost:4567${path}`)
+  .then((resp: Response) => {
+      if (resp.ok) {
+        return
+      } else {
+        throw resp.text()
+      }
+    })
 }
+
+const Main: FC = () => {
+
+  const [ ledState, setLedState ] = useState(false)
+  const [ lcsState, setLcsState ] = useState(false)
+  const [ fanState, setFanState ] = useState(false)
+
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const switchLed = (checked: boolean) =>{
+    console.log("Using LED state:", ledState)
+    let path: string
+    if (checked) {
+      path = '/led/on'
+    } else {
+      path = '/led/off'
+    }
+    sendRequest(path)
+    .then(() => {
+      setLedState(checked)
+    })
+    .catch((e: Error) => {
+      console.log(e)
+      messageApi.error(e.message)
+    })
+  }
+
+  const switchLcs = (checked: boolean)=>{
+    let path: string
+    if (checked) {
+      path = '/lcs/on'
+    } else {
+      path = '/lcs/off'
+    }
+    sendRequest(path)
+    .then(() => {
+      setLcsState(checked)
+    })
+    .catch((e: Error) => {
+      console.log(e)
+      messageApi.error(e.message)
+    })
+  }
+
+  const switchFan = (checked: boolean)=>{
+    let path: string
+    if (checked) {
+      path = '/fan/on'
+    } else {
+      path = '/fan/off'
+    }
+    sendRequest(path)
+    .then(() => {
+      setFanState(checked)
+    })
+    .catch((e: Error) => {
+      console.log(e)
+      messageApi.error(e.message)
+    })
+  }
+
+  return <>
+  <Row justify="center" align='middle' style={{height: '100vh'}}>
+    <Col xs={8}>
+    <div className='paper'>
+      <Typography.Title level={3}>Neose Mini Control Interface</Typography.Title>
+        <Row>
+          <Col>
+          <div className='inner-col'>
+            <span>LED: </span><Switch checked={ledState} onChange={(checked) => {switchLed(checked)}}></Switch>
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <div className='inner-col'>
+            <span>LCS: </span><Switch checked={lcsState} onChange={(checked) => {switchLcs(checked)}}></Switch>
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <div className='inner-col'>
+            <span>FAN: </span><Switch checked={fanState} onChange={(checked) => {switchFan(checked)}}></Switch>
+          </div>
+          </Col>
+        </Row>
+      </div>
+    </Col>
+  </Row>
+</>
+}
+const App: FC = () => (
+  <div className="App">
+    <Main/>
+  </div>
+);
 
 export default App;
